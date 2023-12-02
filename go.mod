e3403bf881a78e1caff0928d9caa301b592fcec1
Erik Eppel <erik@featureform.com>
2023-12-01 16:13:45 -0800

    initial commit; sets up simple advent of code project structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aoc-23
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+type Puzzle interface {
+	Solve() (interface{}, error)
+}
+
+func main() {
+	fmt.Println("Advent of Code 2023!")
+
+	dayNum := flag.Int("day", 1, "selects a day")
+	puzzleNum := flag.Int("puzzle", 1, "selects a day's puzzle")
+
+	flag.Parse()
+
+	if *dayNum <= 0 || *dayNum > 25 {
+		log.Fatalf("day must be an integer between 1 and 25; received %d", *dayNum)
+	}
+
+	if *puzzleNum <= 0 || *puzzleNum >= 3 {
+		log.Fatalf("puzzle must be either a 1 or a 2; received %d", *puzzleNum)
+	}
+
+	var puzzle Puzzle
+	var puzzleErr error
+	switch *dayNum {
+	default:
+		puzzleErr = fmt.Errorf("day %d not implemented", *dayNum)
+	}
+	if puzzleErr != nil {
+		log.Fatalf("failed to get puzzle %d for day %d: %v", *puzzleNum, *dayNum, puzzleErr)
+	}
+
+	solution, err := puzzle.Solve()
+
+	if err != nil {
+		log.Fatalf("failed to solve puzzle %d for day %d: %v", *puzzleNum, *dayNum, err)
+	}
+
+	log.Printf("Solution for puzzle %d of day %d is %v (type %T)", *puzzleNum, *dayNum, solution, solution)
+}
